d7001a5f1c2a3da482a152704838c4dce5da0bf4
alexa <alexa@LAPTOP-GIU00E83.home>
2021-01-25 22:44:55 +0100

    update

--- Projet Sports/src/data/Jogging.java
+++ Projet Sports/src/data/Jogging.java
@@ -2,5 +2,17 @@ package data;
 
 public class Jogging extends Sport
 {
+private int duree_seance;
+private int distance_parcourue;
+
+public Jogging(int duree_seance,int distance_parcourue) {
+	this.duree_seance=duree_seance;
+	this.distance_parcourue=distance_parcourue;
+	
+	
+	
+}
+
+
 
 }
