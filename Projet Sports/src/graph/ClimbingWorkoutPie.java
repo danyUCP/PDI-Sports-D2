01ee5bfacd01ce88d53886a60562bd5b93dbd368
Hike95 <60402524+Hike95@users.noreply.github.com>
2021-02-07 13:08:44 +0100

    ce sont tout les graphe de tout les sports

---
+++ Projet Sports/src/graph/ClimbingWorkoutPie.java
@@ -0,0 +1,52 @@
+package graph;
+
+import javax.swing.JPanel;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.general.DefaultPieDataset;
+import org.jfree.data.general.PieDataset;
+import org.jfree.ui.ApplicationFrame;
+import org.jfree.ui.RefineryUtilities;
+
+public class ClimbingWorkoutPie extends ApplicationFrame  {
+	
+	public ClimbingWorkoutPie(String title) {
+		super(title);
+		setContentPane(createDemoPanel());
+		// TODO Auto-generated constructor stub
+	}
+
+	private static final long serialVersionUID = 1L;
+
+	private PieDataset createDataset() {
+		DefaultPieDataset dataset = new DefaultPieDataset();
+		dataset.setValue("black wall", new Double(25.0));
+		dataset.setValue("red wall", new Double(20.0));
+		dataset.setValue("green wall", new Double(5.0));
+		dataset.setValue("brown wall", new Double(23));
+	
+		return dataset;
+	}
+
+	private JFreeChart createChart(PieDataset dataset) {
+		return ChartFactory.createPieChart("Climbing Workout Pie Chart", dataset, true, true, false);
+	}
+
+	public JPanel createDemoPanel() {
+		JFreeChart chart = createChart(createDataset());
+		return new ChartPanel(chart);
+	}
+
+	public static void main(String[] args) {
+		ClimbingWorkoutPie demo = new ClimbingWorkoutPie("ClimbingWorkoutPie Chart Demo");
+		demo.pack();
+		RefineryUtilities.centerFrameOnScreen(demo);
+		demo.setVisible(true);
+	}
+	
+	
+	
+	
+}
---
+++ Projet Sports/src/graph/SwimmingWorkoutBarChart.java
@@ -0,0 +1,98 @@
+package graph;
+
+import java.awt.Dimension;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.ui.ApplicationFrame;
+import org.jfree.ui.RefineryUtilities;
+/*
+import chart.BarChartDemo;
+*/
+public class SwimmingWorkoutBarChart extends ApplicationFrame  {
+
+	private static final long serialVersionUID = 1L;
+
+	public SwimmingWorkoutBarChart(String title) {
+
+		super(title);
+
+		CategoryDataset dataset = createDataset();
+		JFreeChart chart = createChart(dataset);
+		ChartPanel chartPanel = new ChartPanel(chart);
+		chartPanel.setPreferredSize(new Dimension(500, 270));
+		setContentPane(chartPanel);
+
+	}
+
+	private CategoryDataset createDataset() {
+
+		String series1 = "meterBreaststroke";
+		String series2 = "meterbutterfly";
+		String series3 = "meterCrowl";
+		String series4 = "meterback";
+		
+		String category1 = "session 1";
+		String category2 = "session 2";
+		String category3 = "session 3";
+		String category4 = "session 4";
+		String category5 = "session 5";
+
+		DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+
+		dataset.addValue(2.0, series1, category1);
+		dataset.addValue(4.0, series1, category2);
+		dataset.addValue(3.0, series1, category3);
+		dataset.addValue(5.0, series1, category4);
+		dataset.addValue(5.0, series1, category5);
+
+		dataset.addValue(5.0, series2, category1);
+		dataset.addValue(7.0, series2, category2);
+		dataset.addValue(6.0, series2, category3);
+		dataset.addValue(8.0, series2, category4);
+		dataset.addValue(4.0, series2, category5);
+
+		dataset.addValue(4.0, series3, category1);
+		dataset.addValue(3.0, series3, category2);
+		dataset.addValue(2.0, series3, category3);
+		dataset.addValue(3.0, series3, category4);
+		dataset.addValue(6.0, series3, category5);
+		
+		dataset.addValue(5.0, series4, category1);
+		dataset.addValue(7.0, series4, category2);
+		dataset.addValue(6.0, series4, category3);
+		dataset.addValue(8.0, series4, category4);
+		dataset.addValue(4.0, series4, category5);
+
+		return dataset;
+
+	}
+
+	private JFreeChart createChart(CategoryDataset dataset) {
+
+		JFreeChart chart = ChartFactory.createBarChart("Bar Chart Demo", "Category", "distance", dataset,
+				PlotOrientation.VERTICAL, true, true, false);
+
+		return chart;
+
+	}
+
+	public static void main(String[] args) {
+		SwimmingWorkoutBarChart demo = new SwimmingWorkoutBarChart("SwimmingWorkoutBar Chart");
+		demo.pack();
+		RefineryUtilities.centerFrameOnScreen(demo);
+		demo.setVisible(true);
+	}
+
+	
+	
+	
+	
+}
+
+
+
